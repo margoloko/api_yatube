14cba8f71e6f21d3cf15b2dd1c940a89b3412e4a
Marina <margoloko@mail.ru>
2022-02-12 12:41:11 +0700

    v2

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -10,7 +10,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('id', 'text', 'pub_date', 'author', 'group', 'image')
+        fields = ('__all__')
         read_only_fields = ('group',)
 
 
@@ -28,5 +28,5 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ('id', 'author', 'post', 'text', 'created',)
+        fields = ('__all__')
         read_only_fields = ('post',)
