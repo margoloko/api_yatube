86032b7f70fe2a85de1ae2c52e219f05edd60b1f
Balakhonova Marina <90852680+margoloko@users.noreply.github.com>
2023-03-06 16:58:59 +0300

    Update serializers.py
    add documentation

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -4,7 +4,12 @@ from posts.models import Post, Group, Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
-
+    """Сериализатор PostSerializer содержит следующие поля:
+    author - автор поста (slug связь с моделью User).
+    text - текст поста.
+    pub_date - дата публикации поста.
+    image - изображение поста.
+    group - группа, к которой относится пост."""
     author = serializers.SlugRelatedField(read_only=True,
                                           slug_field='username')
 
@@ -15,14 +20,21 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class GroupSerializer(serializers.ModelSerializer):
-
+    """Сериализатор GroupSerializer содержит следующие поля:
+    title - название группы.
+    slug - уникальный идентификатор группы.
+    description - описание группы."""
     class Meta:
         model = Group
         fields = ('__all__')
 
 
 class CommentSerializer(serializers.ModelSerializer):
-
+    """Сериализатор CommentSerializer содержит следующие поля:
+    author - автор комментария (slug связь с моделью User).
+    post - пост, к которому относится комментарий.
+    text - текст комментария.
+    created - дата добавления комментария."""
     author = serializers.SlugRelatedField(slug_field='username',
                                           read_only=True)
 
