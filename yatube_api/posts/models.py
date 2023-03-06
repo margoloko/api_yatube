1cc06808fb53a3ccee8190fcd3bf785925435522
Balakhonova Marina <90852680+margoloko@users.noreply.github.com>
2023-03-06 16:54:23 +0300

    Update models.py

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -5,6 +5,11 @@ User = get_user_model()
 
 
 class Group(models.Model):
+    """Модель Group описывает группы и 
+    содержит следующие поля:
+    title - название группы (строка, не более 200 символов).
+    slug - уникальный идентификатор группы (slug, строка).
+    description - описание группы (строка)."""
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
@@ -14,6 +19,12 @@ class Group(models.Model):
 
 
 class Post(models.Model):
+    """Модель Post содержит следующие поля:
+    text - текст поста (строка).
+    pub_date - дата публикации поста (дата и время, автоматически заполняется при создании поста).
+    author - автор поста (связь с моделью User).
+    image - изображение поста (опциональное поле для загрузки картинки).
+    group - группа, к которой относится пост (связь с моделью Group)."""
     text = models.TextField()
     pub_date = models.DateTimeField(
         'Дата публикации', auto_now_add=True
@@ -34,6 +45,12 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
+    """Модель Comment описывает комментарии и содержит следующие поля:
+    author - автор комментария (связь с моделью User).
+    post - пост, к которому относится комментарий (связь с моделью Post).
+    text - текст комментария (строка).
+    created - дата добавления комментария 
+    (дата и время, автоматически заполняется при создании комментария)."""
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='comments'
     )
